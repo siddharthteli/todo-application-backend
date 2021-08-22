ae720872d133ac73e18cd50e64b8a1e2d0e9192b
Siddharth Teli <telisiddharth123@gmail.com>
2021-08-23 02:09:26 +0530

    Add: Impl view_one_todo function Implemented:
    Because:

--- src/service/mongo_service.rs
+++ src/service/mongo_service.rs
@@ -2,20 +2,29 @@ use crate::model::*;
 use mongodb::{
     bson::{doc, Document},
     error::Error,
+    Client, Collection,
 };
-pub struct mongo_service;
+pub struct Mongo_service;
 
-impl mongo_service {
-    pub fn view_one_todo(task_id: i64) -> Result<Document, Error> {
+impl Mongo_service {
+    pub async fn connection() -> Result<Collection, Error> {
+        let client = Client::with_uri_str("mongodb://localhost:27017").await?;
+        let collection = client.database("todo").collection("todo_collection");
+        Ok(collection)
+    }
+    pub async fn view_one_todo(task_id: i64) -> Result<Document, Error> {
+        let collection=Mongo_service::connection().await?;
+        let task_id_document = doc! {"task_id":task_id};
+        let todo_document=collection.find_one(task_id_document, None).await?.unwrap();
+        Ok(todo_document)
+    }
+    pub async sfn create_one_todo(task: InsertableUpdatableTodo) -> Result<Document, Error> {
         Ok(doc! {"Placeholder"})
     }
-    pub fn create_one_todo(task: InsertableUpdatableTodo) -> Result<Document, Error> {
+    pub async fn update_one_todo(task: InsertableUpdatableTodo) -> Result<Document, Error> {
         Ok(doc! {"Placeholder"})
     }
-    pub fn update_one_todo(task: InsertableUpdatableTodo) -> Result<Document, Error> {
-        Ok(doc! {"Placeholder"})
-    }
-    pub fn delete_one_todo(task_id: i64) -> Result<bool, Error> {
+    pub async fn delete_one_todo(task_id: i64) -> Result<bool, Error> {
         Ok(true)
     }
 }
