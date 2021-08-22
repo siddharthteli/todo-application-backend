5dc94963ec25fb955980da88662025796cf1cb45
Siddharth Teli <telisiddharth123@gmail.com>
2021-08-22 23:51:59 +0530

    Add:Default catcher Implemented:
    Because:

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-use rocket::*;
+use rocket::{
+    catch, catchers, get,
+    http::Status,
+    launch, routes,
+    serde::json::{json, Value},
+    Request,
+};
 
-#[get("/")]
-fn world() -> &'static str {
-    "Hello"
+#[get("/home")]
+fn home() -> Value {
+    json! {"Hello"}
 }
-
+#[catch(default)]
+fn default(status: Status, req: &Request) -> String {
+    format!("{} ({})", status, req.uri())
+}
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![world])
+    rocket::build()
+        .mount("/", routes![home])
+        .register("/", catchers![default])
 }
