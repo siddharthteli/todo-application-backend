44bbb1fd9daa01f82946afc5ed7557eca28e67d6
Siddharth Teli <telisiddharth123@gmail.com>
2021-08-24 00:19:15 +0530

    Test:Delete & Put fetch request Implemented:
    Because:

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use rocket::{
-    catch, catchers,
+    catch, catchers, delete,
     fairing::{Fairing, Info, Kind},
     get,
     http::{Header, Status},
-    launch,
+    launch, options, post, put,
     response::status,
     routes,
     serde::json::{json, Value},
@@ -19,10 +19,25 @@ use controller::{create_one_todo, delete_one_todo, update_one_todo, view_all_tod
 fn home() -> status::Custom<Value> {
     status::Custom(Status::Ok, json! {"You are accessing home end point"})
 }
+
+#[delete("/home/<_id>")]
+fn home_delete(_id: i32) -> status::Custom<Value> {
+    println!("Valueof someting:::-----{:?}", _id);
+    status::Custom(Status::Ok, json! {"You are accessing home end point"})
+}
+#[put("/home")]
+fn home_put() -> status::Custom<Value> {
+    status::Custom(Status::Ok, json! {"You are accessing home end point"})
+}
+#[post("/home")]
+fn home_post() -> status::Custom<Value> {
+    status::Custom(Status::Ok, json! {"You are accessing home end point"})
+}
 #[catch(default)]
 fn default(status: Status, req: &Request) -> String {
     format!("{} ({})", status, req.uri())
 }
+
 pub struct CORS;
 
 #[rocket::async_trait]
@@ -38,8 +53,10 @@ impl Fairing for CORS {
         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
         response.set_header(Header::new(
             "Access-Control-Allow-Methods",
-            "POST, GET, PATCH, OPTIONS",
+            "POST, GET, PATCH, OPTIONS, DELETE",
         ));
+        response.set_header(Header::new("Access-Control-Request-Method", "DELETE"));
+
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
     }
@@ -51,6 +68,9 @@ fn rocket() -> _ {
             "/",
             routes![
                 home,
+                home_post,
+                home_put,
+                home_delete,
                 view_all_todo,
                 view_one_todo,
                 create_one_todo,
