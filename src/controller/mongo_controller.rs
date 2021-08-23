29aa841c0096055c2291fb702934f5abd050a674
Siddharth Teli <telisiddharth123@gmail.com>
2021-08-23 09:35:03 +0530

    Add:Controller function call to service file. Implemented:
    Because:

--- src/controller/mongo_controller.rs
+++ src/controller/mongo_controller.rs
@@ -7,23 +7,60 @@ use rocket::{
 };
 
 use crate::model::*;
+use crate::service::Mongo_service;
 
 #[get("/view-one-todo", data = "<task_id>")]
-pub fn view_one_todo(task_id: Json<i64>) -> status::Custom<Value> {
-    status::Custom(Status::Ok, json!({"Success":true}))
+pub async fn view_one_todo(task_id: Json<i64>) -> status::Custom<Value> {
+    match Mongo_service::view_one_todo(task_id.into_inner()).await {
+        Ok(result) => status::Custom(
+            Status::Ok,
+            json!({ "Success":true,"message":"view_one_todo is working fine","data":result }),
+        ),
+        Err(e) => status::Custom(
+            Status::NotFound,
+            json!({  "Success":false,"message":"view_one_todo is not working","data":e.to_string() }),
+        ),
+    }
 }
 
 #[post("/create-one-todo", data = "<task>")]
-pub fn create_one_todo(task: Json<InsertableUpdatableTodo>) -> status::Custom<Value> {
-    status::Custom(Status::Ok, json!({"Success":true}))
+pub async fn create_one_todo(task: Json<InsertableUpdatableTodo>) -> status::Custom<Value> {
+    match Mongo_service::create_one_todo(task.into_inner()).await {
+        Ok(result) => status::Custom(
+            Status::Ok,
+            json!({ "Success":true,"message":"view_one_todo is working fine","data":result }),
+        ),
+        Err(e) => status::Custom(
+            Status::NotFound,
+            json!({  "Success":false,"message":"view_one_todo is not working","data":e.to_string() }),
+        ),
+    }
 }
 
 #[put("/update-one-todo", data = "<task>")]
-pub fn update_one_todo(task: Json<InsertableUpdatableTodo>) -> status::Custom<Value> {
-    status::Custom(Status::Ok, json!({"Success":true}))
+pub async fn update_one_todo(task: Json<InsertableUpdatableTodo>) -> status::Custom<Value> {
+    match Mongo_service::update_one_todo(task.into_inner()).await {
+        Ok(result) => status::Custom(
+            Status::Ok,
+            json!({ "Success":true,"message":"view_one_todo is working fine","data":result }),
+        ),
+        Err(e) => status::Custom(
+            Status::NotFound,
+            json!({  "Success":false,"message":"view_one_todo is not working","data":e.to_string() }),
+        ),
+    }
 }
 
 #[delete("/delete-one-todo", data = "<task_id>")]
-pub fn delete_one_todo(task_id: Json<i64>) -> status::Custom<Value> {
-    status::Custom(Status::Ok, json!({"Success":true}))
+pub async fn delete_one_todo(task_id: Json<i64>) -> status::Custom<Value> {
+    match Mongo_service::delete_one_todo(task_id.into_inner()).await {
+        Ok(result) => status::Custom(
+            Status::Ok,
+            json!({ "Success":true,"message":"view_one_todo is working fine","data":result }),
+        ),
+        Err(e) => status::Custom(
+            Status::NotFound,
+            json!({  "Success":false,"message":"view_one_todo is not working","data":e.to_string() }),
+        ),
+    }
 }
